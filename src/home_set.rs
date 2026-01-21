2b6f50121aee342b60c5b5c8544cced832d4566a
kazanefu <kazanefu12@gmail.com>
2026-01-22 05:26:06 +0900

    change despawn when exit gamestate::home

--- src/home_set.rs
+++ src/home_set.rs
@@ -1,22 +1,32 @@
+use crate::GameState;
 use bevy::prelude::*;
+
+pub struct HomeSetPlugin;
+
+impl Plugin for HomeSetPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameState::Home), (setup_camera, setup_scene))
+            .add_systems(OnExit(GameState::Home), despawn_all_home_entities);
+    }
+}
+
+// homeにあるもののマーカー
+#[derive(Component)]
+pub struct HomeMarker;
 
 #[derive(Component)]
 pub struct HomeCamera;
 
 // homeにおけるカメラスポーン
-pub fn setup_camera(mut commands: Commands) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 10.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
+        HomeMarker,
     ));
 }
 
-// homeのカメラをデスポーン
-pub fn despawn_camera(mut commands: Commands, camera: Query<Entity, With<HomeCamera>>) {
-    commands.entity(camera.iter().next().unwrap()).despawn();
-}
-
-pub fn setup_scene(
+fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -25,6 +35,7 @@ pub fn setup_scene(
     commands.spawn((
         Mesh3d(meshes.add(Plane3d::default().mesh().size(50.0, 50.0))),
         MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
+        HomeMarker,
     ));
 
     // Light
@@ -34,5 +45,12 @@ pub fn setup_scene(
             ..default()
         },
         Transform::from_xyz(4.0, 8.0, 4.0),
+        HomeMarker,
     ));
+}
+
+fn despawn_all_home_entities(mut commands: Commands, home_marker: Query<Entity, With<HomeMarker>>) {
+    for entity in home_marker.iter() {
+        commands.entity(entity).despawn();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,10 @@ use pc_status::PcStatusPlugin;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(PcStatusPlugin)
-        .add_plugins(GameRulesPlugin)
+        .add_plugins(PcStatusPlugin) // pc status management
+        .add_plugins(GameRulesPlugin) // game state management
         .add_plugins(InputPlugin)
-        .add_plugins(playscene::playscene_setup::PlaySceneSetupPlugin)
-        .add_systems(
-            OnEnter(GameState::Home),
-            (home_set::setup_camera, home_set::setup_scene),
-        )
-        .add_systems(OnExit(GameState::Home), home_set::despawn_camera)
+        .add_plugins(playscene::playscene_setup::PlaySceneSetupPlugin) // play scene management
+        .add_plugins(home_set::HomeSetPlugin) // home scene setup
         .run();
 }
